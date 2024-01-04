13ec90f3db794ebb9aecd28f9e7c5d1a12f06224
LE Palmer <lpalmer@ucsd.edu>
2024-01-04 11:41:20 -0800

    Created function to start the application

--- server.js
+++ server.js
@@ -9,3 +9,52 @@ const db = mysql.createConnection({
     password: '', 
     database: 'employee_db', 
   });
+
+  // Function to start the application
+function startApp() {
+    inquirer
+      .prompt({
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role',
+          'Exit',
+        ],
+      })
+      .then((answer) => {
+        switch (answer.action) {
+          case 'View all departments':
+            viewDepartments();
+            break;
+          case 'View all roles':
+            viewRoles();
+            break;
+          case 'View all employees':
+            viewEmployees();
+            break;
+          case 'Add a department':
+            addDepartment();
+            break;
+          case 'Add a role':
+            addRole();
+            break;
+          case 'Add an employee':
+            addEmployee();
+            break;
+          case 'Update an employee role':
+            updateEmployeeRole();
+            break;
+          case 'Exit':
+            db.end();
+            console.log("Goodbye!")
+            break;
+        }
+      });
+  }
