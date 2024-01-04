d8237f25ba58c6c06368dc07f8dc01372492d365
LE Palmer <lpalmer@ucsd.edu>
2024-01-04 14:59:27 -0800

    Functions to add departments, roles and employees

--- server.js
+++ server.js
@@ -89,3 +89,125 @@ function viewEmployees(){
         })
 
 }
+
+// Function to add a Department
+
+function addDepartment() {
+  inquirer
+      .prompt ({
+          type: "input",
+          name: "name",
+          message: "What is the name of the new department?"
+      }).then((department)=>{
+          const query= `INSERT INTO department (name) VALUES (?)`
+          db.query(query, department.name, (err, data)=>{
+              if(err) throw err;
+              console.log(`The ${department.name} was successfully added to the department database!`)
+              startApp()
+              })
+      })
+}
+
+
+// Function to add Role
+
+function addRole() {
+  db.query(`SELECT * FROM department`, (err,res)=>{
+      if (err) throw(err)
+      let departments = res.map((department)=>({
+          name: department.name,
+          value:department.id
+  }))
+ 
+  inquirer
+  .prompt([
+      {
+          type: "input",
+          name: "title",
+          message: "What is the name of the role?",
+      },
+      {
+          type: "input",
+          name: "salary",
+          message: "What is the salary of the role?",
+      },
+      {
+          type: "list",
+          name: "department",
+          message: "Which department does the role belong to?",
+          choices: departments
+          
+      },
+  ]).then((role)=>{
+      const query= `INSERT INTO role SET ?`
+      db.query(query, 
+          {
+           title: role.title,
+           salary: role.salary,
+           department_id:role.department   
+          }, (err, res)=>{
+              if(err) throw err
+              console.log(` The ${role.title} was successfully added to the role database`)
+              startApp()
+          })
+  })
+
+})
+}
+
+// Function to add Employee
+function addEmployee() {
+  db.query(`SELECT * FROM role`, (err,res)=>{
+      if (err) throw(err)
+      let roles = res.map((role)=>({
+          name: role.title,
+          value:role.id
+  }))
+  db.query(`SELECT * FROM employee`, (err,res)=>{
+      if (err) throw(err)
+      let managers = res.map((manager)=>({
+          name: `${manager.first_name} ${manager.last_name}`,
+          value:manager.id
+  }))
+  
+      inquirer
+      .prompt([
+          {
+              type: "input",
+              name: "firstName",
+              message: "Enter the employee's first name:",
+          },
+          {
+              type: "input",
+              name: "lastName",
+              message: "Enter the employee's last name:",
+          },
+          {
+              type: "list",
+              name: "roleId",
+              message: "Select the employee role:",
+              choices: roles,
+          },
+          {
+              type: "list",
+              name: "managerId",
+              message: "Select the employee's manager:",
+              choices: managers
+          },
+      ]).then((employee)=>{
+          const query= `INSERT INTO employee SET ?`
+          db.query(query, 
+              {
+                  first_name:employee.firstName,
+                  last_name:employee.lastName,
+                  role_id:employee.roleId,
+                  manager_id:employee.managerId
+              }, (err, res)=>{
+                  if(err) throw err
+                  console.log(`${employee.firstName} was successfully added to the employee database`);
+                  startApp()
+              })
+      })
+  })
+})
+      }
